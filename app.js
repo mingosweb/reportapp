041711a76f8a551a73416edca86742c49a499afc
mingosweb <mingox123@gmail.com>
2016-06-06 22:39:53 -0500

    restaurando 3000

--- app.js
+++ app.js
@@ -97,6 +97,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-server.listen(8080, function () {
+server.listen(3000, function () {
   //console.log('Example app listening on port 3000!');
 });
